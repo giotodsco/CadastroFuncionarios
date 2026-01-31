6542d5d7f937a2d195288e73c0717d21fd641c49
giotodsco <todescogiovanni18@gmail.com>
2026-01-31 19:39:43 -0300

    first commit

---
+++ src/test/java/com/example/CadastroFuncionarios/CadastroFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CadastroFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
