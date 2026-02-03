32acffab775762281a8640984f6b68a2aa6b1b1d
giotodsco <todescogiovanni18@gmail.com>
2026-02-03 16:04:29 -0300

    fix: small adjuster

--- src/main/java/com/example/CadastroFuncionarios/Funcinarios/FuncionarioController.java
+++ src/main/java/com/example/CadastroFuncionarios/Funcinarios/FuncionarioController.java
@@ -2,9 +2,11 @@ package com.example.CadastroFuncionarios.Funcinarios;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class FuncionarioController {
 
     @GetMapping("/rotaB")
