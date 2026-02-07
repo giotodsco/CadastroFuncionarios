e5bf66fbf7261c280918f70aa41f58f887e81273
giotodsco <todescogiovanni18@gmail.com>
2026-02-07 20:27:06 -0300

    feat: cria a classe de serviço dos funcionario, listando todos

--- src/main/java/com/example/CadastroFuncionarios/Funcinarios/FuncionariosService.java
+++ src/main/java/com/example/CadastroFuncionarios/Funcinarios/FuncionariosService.java
@@ -1,4 +1,22 @@
 package com.example.CadastroFuncionarios.Funcinarios;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FuncionariosService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public List<FuncionarioModel> listarTodos(){
+        return funcionarioRepository.findAll();
+    }
+
+
+
+
+
 }
