b36ebe540f785163c4ac6e4c1ea86a5cf89c3a01
giotodsco <todescogiovanni18@gmail.com>
2026-02-07 12:41:43 -0300

    refactor: altera a FuncionarioRepository de classe para interface

---
+++ src/main/java/com/example/CadastroFuncionarios/Funcinarios/FuncionarioRepository.java
@@ -0,0 +1,6 @@
+package com.example.CadastroFuncionarios.Funcinarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
+}
--- src/main/java/com/example/CadastroFuncionarios/Funcinarios/FuncionariosRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.CadastroFuncionarios.Funcinarios;
-
-public class FuncionariosRepository {
-}
