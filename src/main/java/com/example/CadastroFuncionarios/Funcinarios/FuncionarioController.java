631fd8f36dcfd96315b092f4ed51e7bdc39efe12
giotodsco <todescogiovanni18@gmail.com>
2026-02-07 12:17:48 -0300

    feat: uptade controller with mock

--- src/main/java/com/example/CadastroFuncionarios/Funcinarios/FuncionarioController.java
+++ src/main/java/com/example/CadastroFuncionarios/Funcinarios/FuncionarioController.java
@@ -1,17 +1,41 @@
 package com.example.CadastroFuncionarios.Funcinarios;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class FuncionarioController {
 
-    @GetMapping("/rotaB")
-    public String rota(){
-        return "Rota teste";
+    // Adicionar Funcionario (CREATE)
+    @PostMapping("/create")
+    public String createFuncionario(){
+        return "create";
+    }
+
+    // Procurar Funcionario por ID (READ)
+    @GetMapping ("/findid")
+    public String findFuncionarioId(){
+        return "find";
+    }
+
+
+    // Mostrar Funcionarios (READ)
+    @GetMapping ("/all")
+    public String findFuncionariosAll(){
+        return "all";
+    }
+
+    // Alterar dados dos Funcionarios (UPTADE)
+    @PutMapping ("/alterar")
+    public String alterarFuncionario(){
+        return "alterar";
+    }
+
+    // Deletar Funcionario (DELETE)
+
+    @DeleteMapping("/delete")
+    public  String deleteFuncionario(){
+        return "delete";
     }
 
 }
