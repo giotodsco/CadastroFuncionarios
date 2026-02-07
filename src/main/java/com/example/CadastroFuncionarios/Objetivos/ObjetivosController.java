4a91da7fa1081d7f79b522d6ea0ebd121f07ad49
giotodsco <todescogiovanni18@gmail.com>
2026-02-07 12:42:03 -0300

    feat: cria os mock da classe de objetivos

--- src/main/java/com/example/CadastroFuncionarios/Objetivos/ObjetivosController.java
+++ src/main/java/com/example/CadastroFuncionarios/Objetivos/ObjetivosController.java
@@ -1,4 +1,38 @@
 package com.example.CadastroFuncionarios.Objetivos;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("objetivos")
 public class ObjetivosController {
+
+    //  CREATE
+    @PostMapping("/createObj")
+    public String createObj(){
+        return "create";
+    }
+
+    // READ
+    @GetMapping("/getAllObj")
+    public String getAllObj(){
+        return "getAllObj";
+    }
+    // READ PER ID
+    @GetMapping("/getIdObj")
+    public String getIdObj(){
+        return "getIdObj";
+    }
+
+    // UPDATE
+    @PutMapping("/alterarObj")
+    public String alterarObj(){
+        return "alterarObj";
+    }
+
+    // DELETE
+    @DeleteMapping("/deleteObj")
+    public String deleteObj(){
+        return "delete";
+    }
 }
